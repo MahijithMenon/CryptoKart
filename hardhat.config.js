fe5f871dbdcb21ff8a9a765d3cb2789442deabc3
Mahijith <menon.mahijith@gmail.com>
2022-09-02 00:15:03 +0530

    devlop

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require('@nomiclabs/hardhat-waffle');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+module.exports = {
+  solidity: '0.8.4',
+};
---
+++ pages/nft-details.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const nftDetails = () => (
+  <div />
+);
+
+export default nftDetails;
