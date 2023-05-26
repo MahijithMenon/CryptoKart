cac33a5b0134e7cc572c745d4ff56df601a8769e
Mahijith <menon.mahijith@gmail.com>
2023-05-26 10:17:32 +0530

    few bug fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,16 +11,24 @@ import Button from './Button';
 const MenuItems = ({ isMobile, active, setActive }) => {
   const generateLink = (i) => {
     switch (i) {
-      case 0: return '/';
-      case 1: return '/listed-nfts';
-      case 2: return '/my-nfts';
+      case 0:
+        return '/';
+      case 1:
+        return '/listed-nfts';
+      case 2:
+        return '/my-nfts';
 
-      default: return '/';
+      default:
+        return '/';
     }
   };
 
   return (
-    <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && 'flex-col h-full'
+      }`}
+    >
       {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
         <li
           key={i}
@@ -28,7 +36,11 @@ const MenuItems = ({ isMobile, active, setActive }) => {
             setActive(item);
           }}
           className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3
-          ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}
+          ${
+            active === item
+              ? 'dark:text-white text-nft-black-1'
+              : 'dark:text-nft-gray-3 text-nft-gray-2'
+          }`}
         >
           <Link href={generateLink(i)}>{item}</Link>
         </li>
@@ -70,15 +82,27 @@ const Navbar = () => {
             className="flexCenter md:hidden cursor-pointer"
             onClick={() => {}}
           >
-            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
-            <p className="dark:text-white text-nft-black-1 font-semibold text-lg">CryptoKart</p>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg">
+              CryptoKart
+            </p>
           </div>
         </Link>
         <Link href="/">
-          <div
-            className="hidden md:flex"
-            onClick={() => {}}
-          ><Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+          <div className="hidden md:flex">
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="logo"
+            />
           </div>
         </Link>
       </div>
@@ -90,15 +114,16 @@ const Navbar = () => {
             id="checkbox"
             onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
           />
-          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
+          >
             <i className="fas fa-sun" />
             <i className="fas fa-moon" />
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
           </label>
-
         </div>
-        <div className="md:hidden flex ">
-
+        <div className="md:hidden flex">
           <MenuItems active={active} setActive={setActive} />
           <div className="ml-4">
             <ButtonGroup setActive={setActive} router={router} />
@@ -106,40 +131,37 @@ const Navbar = () => {
         </div>
       </div>
       <div className="hidden md:flex ml-2">
-        {isOpen
-          ? (
-            <Image
-              src={images.cross}
-              objectFit="contain"
-              width={20}
-              height={20}
-              alt="close"
-              onClick={() => setIsOpen(false)}
-              className={theme === 'light' && 'filter invert'}
-            />
-          )
-          : (
-            <Image
-              src={images.menu}
-              objectFit="contain"
-              width={25}
-              height={25}
-              alt="menu"
-              onClick={() => setIsOpen(true)}
-              className={theme === 'light' && 'filter invert'}
-            />
-          )}
-        {isOpen && (
-        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
-          <div className="flex-1 p-4">
-            <MenuItems active={active} setActive={setActive} isMobile />
-          </div>
-          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
-            <ButtonGroup setActive={setActive} router={router} />
-          </div>
-        </div>
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="close"
+            onClick={() => setIsOpen(false)}
+            className={theme === 'light' && 'filter invert'}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(true)}
+            className={theme === 'light' && 'filter invert'}
+          />
         )}
-
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+            <div className="flex-1 p-4">
+              <MenuItems active={active} setActive={setActive} isMobile />
+            </div>
+            <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+              <ButtonGroup setActive={setActive} router={router} />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
